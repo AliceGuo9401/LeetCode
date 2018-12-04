f73f4fbb6ad49b25ab21e6a171fa4914038fa55a
AliceGuo9401 <45186543+AliceGuo9401@users.noreply.github.com>
2018-12-04 20:32:02 +0800

    C++泛型算法

---
+++ 26RemoveDuplicatesfromSortedArray0.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int removeDuplicates(vector<int>& nums) {
+    auto endUnique = unique(nums.begin(), nums.end());        //unique算法将相邻的重复项“消除”，并返回指向一个不重复元素之后的位置的迭代器
+    int length(0);
+    length = endUnique - nums.begin();
+    return length;
+}
+
+int main() {
+    vector<int> nums{1, 1, 2};
+    int length = removeDuplicates(nums);
+    for (int i = 0; i < nums.size(); ++i)
+        cout << nums[i] << ' ';
+    cout << endl;
+    cout << length;
+    return 0;
+}
