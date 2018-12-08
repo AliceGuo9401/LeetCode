664e6929d91f1f6f4077fb8dfd9583716eaf5aaf
AliceGuo9401 <45186543+AliceGuo9401@users.noreply.github.com>
2018-12-08 17:25:50 +0800

    cmath

---
+++ 29DivideTwoIntegers0.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+int divide(int dividend, int divisor) {
+    if (divisor == 0 || (dividend == INT32_MIN && divisor == -1)) return INT32_MAX;
+
+    double i = log(fabs(dividend));   //double log(double x)函数，定义在cmath头文件中，底数为e。lnM - ln N = ln(M/N)
+    double j = log(fabs(divisor));    //fabs函数，定义在cmath头文件中，求浮点数的绝对值
+    int quotient = (int)exp(i - j);   //double exp(double x)函数，定义在cmath头文件中，e的x次方。e的ln(M/N)次方
+    return dividend < 0 ^ divisor < 0 ? -quotient : quotient;       //条件运算符 ? :
+}
+int main() {
+    int dividend(7), divisor(-3);
+    int quotient = divide(dividend, divisor);
+    cout << quotient;
+    return 0;
+}
