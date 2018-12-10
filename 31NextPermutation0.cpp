cc3d452cf240d688758cefd4cb2cab26b8bdaee3
AliceGuo9401 <45186543+AliceGuo9401@users.noreply.github.com>
2018-12-10 23:05:32 +0800

    树

---
+++ 31NextPermutation0.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+void nextPermutation(vector<int>& nums) {
+    int i(0), j(0), rear(nums.size() - 1);
+    while (nums[rear - i] <= nums[rear - i - 1] && i < nums.size() - 1)
+        ++i;
+    ++i;                                          //借助“树”思考
+    if ( i != nums.size()) {
+        while (nums[rear - j] <= nums[rear - i])
+            ++j;                                  //找到最小的大于nums[rear - i]的元素
+        int temp = nums[rear - j];
+        nums[rear - j] = nums[rear - i];
+        nums[rear - i] = temp;
+    }
+    sort(nums.end() - i, nums.end());
+    return ;
+}
+int main() {
+    vector<int> nums{2, 1, 3};
+    nextPermutation(nums);
+    for (auto num : nums)                         //C++11标准范围for语句，P114
+        cout << num << ' ';
+    return 0;
+}
