ecd284e01929b85edc90717cf219789f9ce1bd38
AliceGuo9401 <45186543+AliceGuo9401@users.noreply.github.com>
2018-12-14 11:23:33 +0800

    二分查找
    给定一个排序数组和目标值，如果找到目标，则返回它的索引；如果没有，则返回按顺序插入的索引。

---
+++ 35SearchInsertPosition0.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int searchInsert(vector<int>& nums, int target) {
+    int low(0), high(nums.size() - 1);
+    int mid = (low + high) / 2;
+    while (low <= high) {
+        if (nums[mid] > target)
+            high = mid - 1;
+        else if (nums[mid] < target)
+            low = mid + 1;
+        else if (nums[mid] == target)
+            return mid;
+        mid = (low + high) / 2;
+    }
+    return low;   /**/
+}
+
+int main() {
+    vector<int> nums{ };
+    int target(8);
+    int index = searchInsert(nums, target);
+    cout << index;
+    return 0;
+}
