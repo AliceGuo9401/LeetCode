463054120b260a882254f380406a032d42f22e56
AliceGuo9401 <45186543+AliceGuo9401@users.noreply.github.com>
2018-12-28 21:38:53 +0800

    双指针（几何 — 面积）

--- 42TrappingRainWater0.cpp
+++ 42TrappingRainWater0.cpp
@@ -1,43 +1,53 @@
-    int trap(vector<int>& height) {
-        if (height.size() < 3) return 0;
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
 
-        int left(0), right(height.size() - 1), rainWater(0);
-        while (height[left] == 0 && left <= right)
-            ++left;
-        while (height[right] == 0 && left <= right)
-            --right;
+int trap(vector<int>& height) {
+    if (height.size() < 3) return 0;
 
-        int flag(0);
-        while (left <= right) {
-            if (height[left] == height[right]) {
-                flag = height[left];
-                do{
-                    rainWater += flag - height[left];
-                    ++left;
-                }
-                while (height[left] <= flag && left <= right);
-                do{
-                    rainWater += flag - height[right];
-                    --right;
-                }
-                while (height[right] <= flag && left <= right);
-            }
-            else if (height[left] < height[right]) {
-                flag = height[left];
-                do{
-                    rainWater += flag - height[left];
-                    ++right;
-                }
-                while (height[left] <= flag && left <= right);
-            }
-            else if (height[left] > height[right]) {
-                flag = height[right];
-                do{
-                    rainWater += flag - height[right];
-                    --right;
-                }
-                while (height[right] <= flag && left <= right);
-            }
+    int left(0), right(height.size() - 1), rainWater(0), s(0);
+    while (height[left] == 0 && left <= right)
+        ++left;
+    while (height[right] == 0 && left <= right)
+        --right;
+
+    int flag(0);
+    while (left <= right) {
+        if (height[left] == height[right]) {
+            s += (height[left] - flag) * (right - left + 1);
+            flag = height[left];
+            do
+                ++left;
+            while (height[left] <= flag && left <= right);
+            do
+                --right;
+            while (height[right] <= flag && left <= right);
         }
-        return rainWater;
+        else if (height[left] < height[right]) {
+            s += (height[left] - flag) * (right - left + 1);
+            flag = height[left];
+            do
+                ++left;
+            while (height[left] <= flag && left <= right);
+        }
+        else if (height[left] > height[right]) {
+            s += (height[right] - flag) * (right - left + 1);
+            flag = height[right];
+            do
+                --right;
+            while (height[right] <= flag && left <= right);
+        }
     }
+    for (auto i : height)
+        rainWater += i;
+    rainWater = s - rainWater;
+    return rainWater;
+}
+
+int main() {
+    vector<int> height{4, 2, 0, 3, 2, 5};
+    int rain = trap(height);
+    cout << rain;
+    return 0;
+}
