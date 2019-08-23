b23f39d34602016db942f1c0ca86b6ec0b801bb3
AliceGuo9401 <45186543+AliceGuo9401@users.noreply.github.com>
2019-08-23 16:31:26 +0800

    Generic Algorithm-unique
    Remove consecutive duplicates in range.

--- 26RemoveDuplicatesfromSortedArray0.cpp
+++ 26RemoveDuplicatesfromSortedArray0.cpp
@@ -5,7 +5,7 @@
 using namespace std;
 
 int removeDuplicates(vector<int>& nums) {
-    auto endUnique = unique(nums.begin(), nums.end());        //unique算法将相邻的重复项“消除”，并返回指向一个不重复元素之后的位置的迭代器
+    auto endUnique = unique(nums.begin(), nums.end());      //unique算法将相邻的重复项“消除”，并返回指向一个不重复元素之后的位置的迭代器
     int length(0);
     length = endUnique - nums.begin();
     return length;
